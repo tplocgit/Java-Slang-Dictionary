bab4e87a067a794400556b790daa4fe60ce36080
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-11-22 14:04:20 +0700

    Fix data + ADD Menu + function #1: 'Find the definition of slang'

--- src/hcmus/edu/java/project01/Main.java
+++ src/hcmus/edu/java/project01/Main.java
@@ -1,14 +1,30 @@
 package hcmus.edu.java.project01;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static final String WM_PARAMETER = "`";
     public static final String MM_PARAMETER = "|";
     public static final String PATH = "./dict/slang.txt";
+    public static final String[] OPTIONS = {"Search definition", "Search slang", "History", "Add new slang",
+                                            "Edit slang", "Remove slang", "Reset to origin dictionary",
+                                            "On this day slang", "Quiz: Find right definition", "Quiz find right slang",
+                                            "Exit"};
+    public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         SlangDictionary myDict = new SlangDictionary();
         myDict.fromFile(PATH, WM_PARAMETER, MM_PARAMETER);
-        myDict.showAll();
+        myDict.setMenu(OPTIONS);
+        int op;
+        do {
+            myDict.showMenu();
+            op = myDict.getTask();
+            myDict.doTask(op);
+            System.out.print("Press enter key to continue.");
+            input.nextLine();
+        }
+        while (op != OPTIONS.length);
     }
 }
--- src/hcmus/edu/java/project01/SlangDictionary.java
+++ src/hcmus/edu/java/project01/SlangDictionary.java
@@ -5,10 +5,20 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class SlangDictionary {
-    TreeMap<String, LinkedList<String>> dict;
+    private TreeMap<String, LinkedList<String>> dict;
+    private StringBuilder menu;
+    private int ntask;
+    private final Scanner input;
 
     SlangDictionary() {
         this.dict = new TreeMap<>();
+        this.menu = new StringBuilder();
+        this.ntask = 0;
+        this.input = new Scanner(System.in);
+    }
+
+    public LinkedList<String> getDefinition(String slang) {
+        return this.dict.get(slang);
     }
 
     public void addWord(String word, String mean) {
@@ -26,10 +36,10 @@ public class SlangDictionary {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        builder.append("Word\t|\tMeans\n");
+        builder.append("Word: Means\n");
 
         for (Map.Entry<String, LinkedList<String>> entry : this.dict.entrySet()) {
-            builder.append(entry.getKey()).append("\t\t|\t");
+            builder.append(entry.getKey()).append(": ");
 
             Iterator<String> it = entry.getValue().iterator();
             while (it.hasNext()) {
@@ -45,7 +55,55 @@ public class SlangDictionary {
         return builder.toString();
     }
 
-    public void showAll() {
+    public void setMenu(String[] options) {
+        this.menu.delete(0, this.menu.length());
+        this.menu = new StringBuilder();
+        for (int i = 0; i < options.length; ++i)
+            this.menu.append(i + 1).append(". ").append(options[i]).append("\n");
+        ntask = options.length;
+    }
+
+    public String getMenu() {
+        return this.menu.toString();
+    }
+
+    public void showMenu() {
+        System.out.println(this.getMenu());
+    }
+
+    public int getTask() {
+        Scanner input = new Scanner(System.in);
+        int op;
+        System.out.print("Please enter your choose: ");
+        while(true) {
+            try {
+                op = input.nextInt();
+                if (op <= 0 || op > this.ntask)
+                    continue;
+                break;
+            } catch (Exception ignored){
+                System.out.print("Invalid input. Try again: ");
+            }
+        }
+
+        return op;
+    }
+
+    public void doTask(int taskNum) {
+        if (taskNum == 1) {
+            System.out.print("Enter slang to find definition: ");
+            String slang = this.input.nextLine();
+            LinkedList<String> def = this.getDefinition(slang);
+            System.out.println("Definition of " + slang + " is: " + def.toString());
+        } else if (taskNum == this.ntask) {
+            System.out.println("See you later.");
+        } else {
+            System.out.println("This function hasn't been implemented yet. " +
+                    " Please try another function on our current menu.");
+        }
+    }
+
+    public void showAllSalng() {
         System.out.println(this.toString());
     }
 
@@ -66,6 +124,7 @@ public class SlangDictionary {
                 StringTokenizer tokens = new StringTokenizer(data, word_mean_parameter);
                 String word = tokens.nextToken();
                 String means = tokens.nextToken();
+                //System.out.println(data);
                 tokens = new StringTokenizer(means, mean_mean_parameter);
                 while(tokens.hasMoreTokens()) {
                     String mean = tokens.nextToken().trim();
